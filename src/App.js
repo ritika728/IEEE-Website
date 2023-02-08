ee19ebba11ccca5d7c4fe75505c9f781301accbc
Ritika Malik <ritikamalik100102@gmail.com>
2023-02-08 23:35:06 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import NAVBAR from './components/navbar/navbar.jsx'
-import ABOUT from './components/about/about.jsx'
 
 function App() {
   return (
     <div className="App">
       {<NAVBAR/>}
-      
-      {<ABOUT/>}
     </div>
   );
 }
