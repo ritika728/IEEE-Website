bea5246a25a4491ab7c73e52f92d1c86cbf6b7f6
Ritika Malik <ritikamalik100102@gmail.com>
2023-02-11 19:03:22 +0530

    about section updated

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,17 +12,17 @@ const about = () => {
 <div className='info'>
 <div className='container reveal fadeLeft'>
   <div className='ieee'>
-    <div className='ieeetext'><p>Institute of Electrical and Electronics Engineers(IEEE) is an association dedicated to advancing innovation and technological excellence for the benefit of humanity.</p></div>
+    <div className='ieeetext'><strong><p style={{fontSize:"25px" , textTransform:"uppercase", color:"rgb(204,214,246)" }}>Institute of Electrical and Electronics Engineers(IEEE)</p></strong> An association dedicated to advancing innovation and technological excellence for the benefit of humanity.</div>
     <img className="ieee_im"  style={{height:"130px"}}src={require('./ieee2.png')} alt="alternate" />
   </div></div>
   <div className='container reveal faderight'>
   <div className='rsc'>
     <img className="rsc_im"  style={{height:"130px"}}src={require('./ieee2.png')} alt="alternate" />
-    <div className='rsctext'><p>A daylong event intended to attract inquisitive minds from across the state to interact, participate and discuss the evolving and changing technology.</p></div>
+    <div className='rsctext'><strong><p style={{fontSize:"25px" , textTransform:"uppercase", color:"rgb(204,214,246)" }}>RAJASTHAN SUB-SECTION CONGRESS (RSSC)</p></strong>A daylong event intended to attract inquisitive minds from across the state to interact, participate and discuss the evolving and changing technology.</div>
   </div></div>
   <div className='container reveal fadeLeft'>
   <div className='must'>
-    <div className='musttext'><p>IEEE Student Branch, Mody University of Science and Technology provides opportunities to connect, meet and learn from fellow IEEE students and engage with professional IEEE members locally. </p></div>
+    <div className='musttext'><strong><p style={{fontSize:"23px" , textTransform:"uppercase", color:"rgb(204,214,246)" }}>IEEE Student Branch Mody University of Science and Technology</p></strong> Provides a platform & opportunities to connect, meet and learn from fellow IEEE students and engage with professional IEEE members locally.</div>
     <img className="must_im"  style={{height:"130px"}}src={require('./ieee2.png')} alt="alternate" />
   </div></div>
 </div>
